034c080147af40241630e6dc3d0af9fc0c6beba2
Lyon <lyon.b.lee@gmail.com>
2023-01-15 22:57:04 -0500

    Add special statement for when spot = 0 or 1

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,11 +8,13 @@ export default function DayListItem(props) {
     'day-list__item--selected' : props.selected,
     'day-list__item--full' : !props.spots
   })
-
+  
   return (
     <li className={dayListItemClass} onClick={()=> props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
+      {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
+      {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
     </li>
   );
 }
