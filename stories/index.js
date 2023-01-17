c90eb395c7d38b17c3e3a39816f8b4b5ac698ec3
Lyon <lyon.b.lee@gmail.com>
2023-01-16 21:29:20 -0500

    Refactor InterviewerList and InterviewerListItem component

--- stories/index.js
+++ stories/index.js
@@ -130,9 +130,10 @@ storiesOf("Button", module)
       />
     ))
     .add("Clickable", () => (
-      <InterviewerList
-        interviewers={interviewers}
-        setInterviewer={action("setInterviewer")}
+      <InterviewerListItem
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={() => action("setInterviewer")(interviewer.id)}
       />
     ));
       
