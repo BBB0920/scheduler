a4343f6ba8e12eec734050d01c49541819f636c2
Lyon <lyon.b.lee@gmail.com>
2023-01-17 21:15:09 -0500

    Bring all the appointment components together and add them to application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
@@ -20,9 +21,58 @@ const days = [
   },
 ];
 
+
+// Mock data
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
-  
+  const appointmentList = Object.values(appointments).map(appointment => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}    // Spreading: All the object key names match prop names
+      />
+    )
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -42,6 +92,8 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         <nav>
+          {appointmentList}
+          <Appointment key="last" time="5pm" />
         </nav>
       </section>
     </main>
